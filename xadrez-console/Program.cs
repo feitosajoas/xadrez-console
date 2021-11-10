1bb682caf85c5a3f3bfed6242fdd1ea1b3469afd
feitosajoas <joasfeitosadelimasilva@gmail.com>
2021-11-10 11:18:53 -0300

    Implementando passagem de turno e jogador

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,6 +16,9 @@ namespace xadrez_console
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
